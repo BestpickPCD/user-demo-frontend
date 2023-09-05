5e0918711014426f6aa96d941bafcec603ba42c2
PCD <pascalpcd@bestpicktechnology.com>
2023-09-05 17:57:33 +0700

    signin

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import SignInSide from './registeration/index.tsx';
+import { Container } from '@mui/material';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Container>
+      <SignInSide />
+    </Container>
   </React.StrictMode>
 );
 
