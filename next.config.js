35daf98c0c02ddd3b07002f785fc644b90248df6
PCD <pascalpcd@bestpicktechnology.com>
2023-09-25 16:23:41 +0700

    get games

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["ap-south-1.linodeobjects.com"],
+    domains: ["ap-south-1.linodeobjects.com","https://user-demo-frontend.vercel.app/"],
+
   },
 };
 
