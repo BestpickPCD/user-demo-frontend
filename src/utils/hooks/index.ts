b278ee68e5ac5f674e0daa352cf9273b1d299c41
PCD <pascalpcd@bestpicktechnology.com>
2023-10-12 11:25:50 +0700

    wrong file name

--- src/utils/hooks/index.ts
+++ src/utils/hooks/index.ts
@@ -1,3 +1,3 @@
 export { default as useModal } from "./useModal";
 export { default as useToast } from "./useToast";
-export { default as useClickOutside } from "./useClickoutSide";
+export { default as useClickOutside } from "./useClickOutSide";
